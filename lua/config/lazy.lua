c7fce80e563993176163d540554731797026d823
Christian Mortensen <cnmrtnsn@gmail.com>
2025-04-19 23:36:12 +0200

    Add some colorschemes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,8 @@ require("lazy").setup({
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
 	--install = { colorscheme = { "habamax" } },
+	--install = { colorscheme = { "dracula" } },
+	--install = { colorscheme = { "catppuccin-macchiato" } },
 	install = { colorscheme = { "dracula" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,43 @@
 return {
-    {
-        'dracula/vim', as = 'dracula', -- Dracula theme
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme("dracula")
-        end
-    },
+	{
+		"dracula/vim",
+		-- "Mofiqul/dracula.nvim",
+		name = "dracula",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("dracula")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("catppuccin-macchiato")
+		-- end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		priority = 1000,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("tokyonight")
+		-- end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa",
+		priority = 1000,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("kanagawa")
+		-- end,
+	},
+	{
+		"NTBBloodbath/doom-one.nvim",
+		name = "doom-one",
+		priority = 1000,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("doom-one")
+		-- end,
+	},
 }
