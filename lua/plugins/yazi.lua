a633f21565a5d55d717b3e674c6aeab8c5eaea17
Christian Mortensen <cnmrtnsn@gmail.com>
2025-04-23 22:43:30 +0200

    Add yazi nvim plugin

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,38 @@
+return {
+	"mikavilpas/yazi.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"folke/snacks.nvim",
+	},
+	keys = {
+		{
+			"<leader>Y",
+			-- mode = { "n", "v" },
+			"<cmd>Yazi<cr>",
+			desc = "yazi at the current file",
+		},
+		{
+			"<leader>y",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Yazi at cwd",
+		},
+		{
+			"<c-up>",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume the last yazi session",
+		},
+	},
+	opts = {
+		-- if you want to open yazi instead of netrw, see below for more info
+		open_for_directories = true,
+		keymaps = {
+			show_help = "<f1>",
+		},
+	},
+	-- 👇 if you use `open_for_directories=true`, this is recommended
+	init = function()
+		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+		-- vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+	end,
+}
